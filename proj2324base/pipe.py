00e98d5ebaf7a8a4912b8e2dc8dcee0c434c832d
Irell Zane <itsirellzane@gmail.com>
2024-04-25 09:01:46 +0100

    Add base files

---
+++ proj2324base/pipe.py
@@ -0,0 +1,113 @@
+# pipe.py: Template para implementação do projeto de Inteligência Artificial 2023/2024.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 00:
+# 00000 Nome1
+# 00000 Nome2
+
+import sys
+from search import (
+    Problem,
+    Node,
+    astar_search,
+    breadth_first_tree_search,
+    depth_first_tree_search,
+    greedy_search,
+    recursive_best_first_search,
+)
+
+
+class PipeManiaState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = PipeManiaState.state_id
+        PipeManiaState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    # TODO: outros metodos da classe
+
+
+class Board:
+    """Representação interna de um tabuleiro de PipeMania."""
+
+    def get_value(self, row: int, col: int) -> str:
+        """Devolve o valor na respetiva posição do tabuleiro."""
+        # TODO
+        pass
+
+    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
+        """Devolve os valores imediatamente acima e abaixo,
+        respectivamente."""
+        # TODO
+        pass
+
+    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
+        """Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente."""
+        # TODO
+        pass
+
+    @staticmethod
+    def parse_instance():
+        """Lê o test do standard input (stdin) que é passado como argumento
+        e retorna uma instância da classe Board.
+
+        Por exemplo:
+            $ python3 pipe.py < test-01.txt
+
+            > from sys import stdin
+            > line = stdin.readline().split()
+        """
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+class PipeMania(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        # TODO
+        pass
+
+    def actions(self, state: PipeManiaState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        # TODO
+        pass
+
+    def result(self, state: PipeManiaState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+        # TODO
+        pass
+
+    def goal_test(self, state: PipeManiaState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas de acordo com as regras do problema."""
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro do standard input,
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    pass
