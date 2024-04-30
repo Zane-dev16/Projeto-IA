0767022d0a10dcd546a490c07e6a7f075abc3332
Joana-Vaz <joana.catarina.fernandes.vaz@tecnico.ulisboa.pt>
2024-04-30 20:58:10 +0100

    isto nao tem alteracoes no codigo apenas nos comentarios que era preciso meter

--- proj2324base/pipe.py
+++ proj2324base/pipe.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 17:
+# 107161 Irell Zane
+# 106078 Joana Vaz
 
 import sys
 from search import (
